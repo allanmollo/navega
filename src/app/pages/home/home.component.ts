1b87ef62f7cac0c37fd41ef43f915ce7b9c3bdd4
unknown <allan.mollo@gmail.com>
2024-10-20 23:06:39 -0300

    feat: adição da página home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AccordionModule } from 'primeng/accordion';
+import { ButtonModule } from 'primeng/button';
+import { ChartModule } from 'primeng/chart';
+import data from './data/data.json'
+import { SideMenuComponent } from '../layout/side-menu/side-menu.component';
+import { BaseMobileCheckComponent } from 'src/app/shared/components/mobile-check/mobile-check.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [AccordionModule, ButtonModule, ChartModule, CurrencyPipe, SideMenuComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent extends BaseMobileCheckComponent implements OnInit, OnDestroy {
+
+  data: any;
+  options: any;
+
+  constructor(){
+    super();
+  }
+
+  ngOnInit(): void {
+    this.initWindowResizeListener();
+    this.data = data;
+
+    this.options = {
+      cutout: '70%',
+      responsive: false,
+      maintainAspectRatio: false
+    };
+  }
+
+  ngOnDestroy(): void {
+    this.removeWindowResizeListener();
+  }
 
 }
