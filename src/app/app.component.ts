ab759b06ee68d1c3b60781c241c2a37eb6ede3d6
unknown <allan.mollo@gmail.com>
2024-10-20 23:09:55 -0300

    feat: inclusão do header no app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './pages/layout/header/header.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'navega';
+  showHeader: boolean = false;
+
+  constructor(private router: Router) {
+    router.events.subscribe((val: any) => {
+      if(val instanceof NavigationEnd) {
+        if(val.url == '/' || val.url == '/login') {
+          this.showHeader = false;
+        } else {
+          this.showHeader = true;
+        }
+      }
+    })
+  }
 }
